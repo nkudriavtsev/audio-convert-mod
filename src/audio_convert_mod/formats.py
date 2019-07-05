7f1700bb14cb6254e5459e6868f7d9f48a055537
Nicholas Kudriavtsev <nkudriavtsev@gmail.com>
2019-07-05 21:03:40 +0300

    opus has no progress percent counter

--- src/audio_convert_mod/formats.py
+++ src/audio_convert_mod/formats.py
@@ -405,9 +405,9 @@ class opus(object):
   def encode(self, filename, newname, quality):
     """Encodes a new OPUS file"""
     if MSWINDOWS:
-      command = 'opusenc.exe --bitrate %(a)i "%(b)s" "%(c)s" 2>&1 | awk.exe -vRS="\\r" "!/^[ ]+/{gsub(/%%/,\\" \\");if (NR>2) print $2/100;fflush();}"' % {'a': quality, 'b': filename, 'c': newname}
+      command = 'opusenc.exe --bitrate %(a)i "%(b)s" "%(c)s"' % {'a': quality, 'b': filename, 'c': newname}
     else:
-      command = "opusenc --bitrate %(a)i '%(b)s' '%(c)s' 2>&1 | awk -vRS='\\r' '!/^[ ]+/{gsub(/%%/,\" \");if (NR>2) print $2/100;fflush();}'" % {'a': quality, 'b': filename, 'c': newname}
+      command = "opusenc --bitrate %(a)i '%(b)s' '%(c)s'" % {'a': quality, 'b': filename, 'c': newname}
     sub = subprocess.Popen(command, shell=True, env=environ, stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     return sub, command
 
